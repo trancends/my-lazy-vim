8887ccc05560198f31ae2a344fa42709069e88f9
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-08-24 18:18:18 +0700

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.scrolloff = 15
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+  { "haishanh/night-owl.vim" },
+  { "EdenEast/nightfox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
+    },
+  },
+}
