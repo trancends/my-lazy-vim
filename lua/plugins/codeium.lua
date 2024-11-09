4b5d07494ee32031504d2205f9733187e3df408e
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-11-09 12:24:29 +0700

    add mini-surround

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,9 +1,9 @@
 return {
   "Exafunction/codeium.vim",
   config = function()
-    vim.keymap.set("i", "<c-cr>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
+    -- vim.keymap.set("i", "<c-cr>", function()
+    --   return vim.fn["codeium#Accept"]()
+    -- end, { expr = true, silent = true })
     vim.keymap.set("i", "<c-;>", function()
       return vim.fn["codeium#CycleCompletions"](1)
     end, { expr = true, silent = true })
